f436637478632ad777426bef2bbcc28dda1f13b0
alexandriajordyn721-yYjTqo <alexandria.jordyn721@gmail.com>
2022-05-16 18:42:14 -0400

    Update

--- Linux/backup.sh
+++ Linux/backup.sh
@@ -3,10 +3,10 @@
 #Create /var/backup 
 mkdir -p /var/backup
 
-#Create new /var/backup/backup.tar
+#Create new /var/backup/backup.tar to home directory
 tar cvf /var/backup/home_backup.tar /home
 
-# Move the file `/var/backup/home.tar` to file with backup save date
+#Move the file `/var/backup/home.tar` to file with backup save date
 mv /var/backup/home_backup.tar /var/backup/home_backup.05102022.tar
 
 #List all files with details in `/var/backup` to a text file called backup_report.txt
