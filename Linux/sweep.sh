4da31f0717594b80ea6a242ed8f396a6cbf406b0
alexandriajordyn721-yYjTqo <alexandria.jordyn721@gmail.com>
2022-05-16 18:42:06 -0400

    Update

--- Linux/sweep.sh
+++ Linux/sweep.sh
@@ -4,9 +4,9 @@
 rm -rf /tmp/*
 rm -rf /var/tmp/*
 
-# Clear apt update
+#Clear apt update
 apt clean -y
 
-# Clear thumbnail for user_name(enter user's account name)
+#Clear thumbnail for user_name(enter user's account name)
 rm -rf /home/user_name.cache/thumbnails
 rm -rf /root/.cache/thumbnails
